e535236ca1408b347cc6c190447aa4b2aa818f31
insert github real name here <insert github email here>
2022-04-20 08:06:36 +0700

    1234

--- Exam/test2/Candies.cpp
+++ Exam/test2/Candies.cpp
@@ -1,22 +1,30 @@
+//Candies Camp2/test2 2564
 #include <bits/stdc++.h>
 #define N 10010
 using namespace std;
 
-int n;
-vector<int> arr(N) ,cand(N ,1);
+int n,ans;
+vector<int> arr(N) ,cand(N ,1) ,dp(N ,1);
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
     cin >> n;
     for(int i=1;i<=n;i++) cin >> arr[i];
 
+    for(int i=n-1;i>=1;i--){
+        if(arr[i] > arr[i+1]) dp[i] = dp[i+1] + 1;
+        else dp[i] = max(dp[i+1] - 1 ,1);
+    }
+
     for(int i=2;i<=n;i++){
         if(arr[i] > arr[i-1]) cand[i] = cand[i-1] + 1;
-        else cand[i] = cand[i-1] - 1;
+        else cand[i] = min(dp[i] ,cand[i-1] - 1);
     }
+
+    for(int i=1;i<=n;i++) ans += cand[i];
+    cout << ans;
 }
 /*
 3
